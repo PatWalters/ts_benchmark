8c475258010f60f0209e9143a120d30b835fbb59
Patrick Walters <wpwalters@gmail.com>
2024-12-04 22:02:04 -0500

    added benchmark.py

--- build_db.py
+++ build_db.py
@@ -5,10 +5,13 @@ import sys
 import pandas as pd
 from sqlitedict import SqliteDict
 from tqdm.auto import tqdm
+from glob import glob
 
 chunksize = 1e5
 
-for filename in sys.argv[1:]:
+file_list = glob("docking/*scores.csv") + glob("rocs/*scores.csv") 
+
+for filename in file_list:
     print(f"processing {filename}")
     db = SqliteDict(filename.replace(".csv",".sqlite"))
     for idx, df in tqdm(enumerate(pd.read_csv(filename,chunksize=chunksize))):
