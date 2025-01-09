c99eff21387cb96e5a7ab993dbcaf91a7704793c
Patrick Walters <wpwalters@gmail.com>
2025-01-08 22:29:34 -0500

    updates for 10 runs

--- benchmark.py
+++ benchmark.py
@@ -6,24 +6,31 @@ import os
 
 ts_command = "/Users/pwalters/software/TS/ts_main.py"
 rws_command = "/Users/pwalters/software/TS_Enhanced/src_multiprocess/ts_main.py"
+ga_command = "/Users/pwalters/software/TS/ga_sampling.py"
 
 os.chdir("docking")
-ts_json_list = glob("ts*.json") 
+ts_json_list = glob("ts*.json")
 rws_json_list = glob("rws*.json")
+ga_json_list = glob("ga*.json") 
 for ts_json_file in ts_json_list:
     subprocess.run([ts_command, ts_json_file])
 for rws_json_file in rws_json_list:
     subprocess.run([rws_command, rws_json_file])
+#for ga_json_file in ga_json_list:
+#    subprocess.run([ga_command, ga_json_file])
     
 os.chdir("..")
 
 os.chdir("rocs")
 ts_json_list = glob("ts*.json") 
 rws_json_list = glob("rws*.json")
+ga_json_list = glob("ga*.json") 
 for ts_json_file in ts_json_list:
     subprocess.run([ts_command, ts_json_file])
 for rws_json_file in rws_json_list:
     subprocess.run([rws_command, rws_json_file])
+#for ga_json_file in ga_json_list:
+#    subprocess.run([ga_command, ga_json_file])
     
     
     
