9d00cb56591c9aa63e42d0be8afe1b627cdbf3aa
Patrick Walters <wpwalters@gmail.com>
2025-01-08 22:33:19 -0500

    added benchmark_10x.py

---
+++ benchmark_10x.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+from glob import glob
+import subprocess
+import os
+
+ts_command = "/Users/pwalters/software/TS/ts_main.py"
+rws_command = "/Users/pwalters/software/TS_Enhanced/src_multiprocess/ts_main.py"
+
+os.chdir("docking")
+ts_json_list = glob("ts*_0*.json")
+rws_json_list = glob("rws*_0*.json")
+for ts_json_file in ts_json_list:
+    subprocess.run([ts_command, ts_json_file])
+for rws_json_file in rws_json_list:
+    subprocess.run([rws_command, rws_json_file])
+    
+os.chdir("..")
+
+os.chdir("rocs")
+ts_json_list = glob("ts*_0*.json") 
+rws_json_list = glob("rws*_0*.json")
+for ts_json_file in ts_json_list:
+    subprocess.run([ts_command, ts_json_file])
+for rws_json_file in rws_json_list:
+    subprocess.run([rws_command, rws_json_file])
+
+os.chdir("..")    
+    
+    
+    
